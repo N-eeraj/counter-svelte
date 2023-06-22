13160de9d23a59d8ec1a08455dde83667cddd9d4
N-eeraj <neeraj7rajeena@gmail.com>
2023-06-23 00:11:35 +0530

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  let count = 0
+  const decrementCount = () => --count
+  const incrementCount = () => ++count
+</script>
+
+<div>
+  <button on:click={decrementCount}>
+    -
+  </button>
+  <span>
+    {count}
+  </span>
+  <button on:click={incrementCount}>
+    +
+  </button>
+</div>
+
+<style>
+</style>
